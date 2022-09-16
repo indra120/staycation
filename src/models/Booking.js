b023304209b6a7a1417f1693145eafcec5ceb014
Indra Pranata <indrapranata120@gmail.com>
2022-09-16 10:14:02 +0800

    Create booking schema

---
+++ src/models/Booking.js
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose'
+
+const bookingSchema = new mongoose.Schema({
+  bookingStartDate: {
+    type: Date,
+    required: true,
+  },
+  bookingEndDate: {
+    type: Date,
+    required: true,
+  },
+  invoice: {
+    type: String,
+    required: true,
+  },
+  itemId: {
+    _id: {
+      type: Object,
+      ref: 'Item',
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  memberId: {
+    type: Object,
+    ref: 'Member',
+  },
+  bankId: {
+    type: Object,
+    ref: 'Bank',
+  },
+  payments: {
+    proofPayment: {
+      type: String,
+      required: true,
+    },
+    bankFrom: {
+      type: String,
+      required: true,
+    },
+    accountHolder: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: 'Proses',
+    },
+  },
+})
+
+export default mongoose.models || mongoose.model('Booking', bookingSchema)
