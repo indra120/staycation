11a75b3946e8b2a396db0224c759c4d61b155e1e
Indra Pranata <indrapranata120@gmail.com>
2022-09-17 22:09:34 +0800

    Create header for admin dashboard

---
+++ src/components/admin/Header.jsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+
+export default function Header({ username }) {
+  return (
+    <nav className='navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow'>
+      <button
+        id='sidebarToggleTop'
+        className='btn btn-link d-md-none rounded-circle mr-3'
+      >
+        <i className='fa fa-bars'></i>
+      </button>
+
+      <ul className='navbar-nav ml-auto'>
+        <div className='topbar-divider d-none d-sm-block'></div>
+
+        <li className='nav-item dropdown no-arrow'>
+          <Link
+            className='nav-link dropdown-toggle'
+            href='#'
+            id='userDropdown'
+            role='button'
+            data-toggle='dropdown'
+            aria-haspopup='true'
+            aria-expanded='false'
+          >
+            <span className='mr-2 d-none d-lg-inline text-gray-600 small'>
+              {username}
+            </span>
+          </Link>
+
+          <div
+            className='dropdown-menu dropdown-menu-right shadow animated--grow-in'
+            aria-labelledby='userDropdown'
+          >
+            <Link className='dropdown-item' href='/admin/logout'>
+              <i className='fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400'></i>
+              Logout
+            </Link>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  )
+}
